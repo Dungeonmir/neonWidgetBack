5be80971e85c3cafa816c2ebcb0102a040b1c020
Sergey <dungeonmir@gmail.com>
2023-06-06 21:43:51 +0300

    server data from google sheets

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const cors = require("cors")
+const { google } = require("googleapis")
+const app = express()
+
+// настройки cors
+app.use(
+	cors({
+		origin: "*",
+	})
+)
+
+const sheets = google.sheets({
+	version: "v4",
+	auth: process.env.GOOGLE_API_KEY,
+})
+
+// Путь по умолчанию
+app.get("/", async (req, res) => {
+	try {
+		res.json({
+			property: "Backend is working",
+		})
+	} catch (error) {
+		console.error(error)
+		res.status(500).send("Internal Server Error")
+	}
+})
+
+// Путь для получения данных
+app.get("/data", async (req, res) => {
+	try {
+		// Получение данных из Google Sheets API
+		const response = await sheets.spreadsheets.values.get({
+			spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+			range: "A1:Z100",
+		})
+
+		// Отправка данных на виджет
+		res.json(response.data.values)
+	} catch (error) {
+		console.error(error)
+		res.status(500).send("Internal Server Error")
+	}
+})
+
+// Запуск сервера
+const port = 3000
+app.listen(port, () => {
+	console.log("Server started on port " + port)
+})
